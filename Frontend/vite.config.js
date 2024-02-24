63173689355153a615ed275946bafcb2201e7a17
Harshit <harshitsingharya24@gmail.com>
2024-02-24 22:57:37 +0530

    updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,12 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     proxy: {
-      '/api': {
-        target: 'https://backend-c29n.vercel.app/',
-        secure: false,
-      },
     },
-    
   },
   plugins: [react()],
 });
