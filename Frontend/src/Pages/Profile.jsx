9bb0097667116f202e1220032baf3d0aade9d6b8
Harshit Singh Arya <136834314+Harsh0906i@users.noreply.github.com>
2024-02-26 21:32:50 +0530

    Update Profile.jsx

--- Frontend/src/Pages/Profile.jsx
+++ Frontend/src/Pages/Profile.jsx
@@ -126,14 +126,13 @@ export default function Profile() {
       setshowListingErr(false)
       const res = await fetch(`https://backend-c29n.vercel.app/api/user/listing/${currentUser._id}`);
       const data = await res.json();
-      console.log("c : ",currentUser._id)
-      console.log(data)
       if (data.success === false) {
         setshowListingErr(true);
         return;
       }
+       const token = response.headers.get('Authorization');
       setuserListing(data)
-      console.log(data);
+      console.log(data,"t : ",token);
     } catch (error) {
       setshowListingErr(true);
     }
